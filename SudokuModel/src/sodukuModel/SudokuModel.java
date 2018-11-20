903f4362584316d57ae356dd8c62506e619b33ce
ELGIGANTEN <ELGIGANTEN@LAPTOP-0FJEI25J>
2018-11-20 18:00:11 +0100

    Pulling and pushing funkar inte

--- SudokuModel/src/sodukuModel/SudokuModel.java
+++ SudokuModel/src/sodukuModel/SudokuModel.java
@@ -2,46 +2,37 @@ package sodukuModel;
 
 public class SudokuModel {
 	private int[][] matrix;
-
+	
 	public SudokuModel() {
-		matrix = new int[9][9];
-
+		matrix=new int[9][9];
+		
 	}
-
-	public boolean next() {
+	
+	public boolean hasNext(int i, int j) {
+		if(true) {
+			
+		}
 		return true;
 	}
-
+	
 	public int getValue(int i, int j) {
 		return matrix[i][j];
 	}
-
+	
 	public void setValue(int i, int j, int value) {
-		matrix[i][j] = value;
+		matrix[i][j]=value;
 	}
-
+	
 	public void finalSolve() {
-		if (solve(0, 0)) {
-//			sätt in alla värden i grafiska interfacet
-		}
-		
+		solve(0,0);
 	}
-
+	
 //	Ska för ruta (i,j) kolla om det finns en lösning, om så finns ska denna 
 	private boolean solve(int i, int j) {
-		
-		
-		if(matrix[i][j] != 0) {
-			if(i<8) {
-				solve(i+1, j);
-			} else if (j<8) {
-				solve(0, j+1);
-			} else {
-				return true;
-			}
+		if(getValue(i, j) == 0) {
+			
 		}
-		
-
+		return true;
 	}
 	
 }
