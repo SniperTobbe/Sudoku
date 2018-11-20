fc85309a1fd08b4172c81d4975a1e5dbcc2a218f
ELGIGANTEN <ELGIGANTEN@LAPTOP-0FJEI25J>
2018-11-20 17:14:46 +0100

    Ramar till startmetoder

--- SudokuModel/src/sodukuModel/SudokuModel.java
+++ SudokuModel/src/sodukuModel/SudokuModel.java
@@ -1,9 +1,32 @@
 package sodukuModel;
 
 public class SudokuModel {
+	private int[][] matrix;
 	
-	public static void main(String[] args) {
-		System.out.println("hej Tobbe");
+	public SudokuModel() {
+		matrix=new int[9][9];
+		
 	}
-
+	
+	public boolean next() {
+		return true;
+	}
+	
+	public int getValue(int i, int j) {
+		return matrix[i][j];
+	}
+	
+	public void setValue(int i, int j, int value) {
+		matrix[i][j]=value;
+	}
+	
+	public void solve() {
+		solve(0,0);
+	}
+	
+//	Ska för ruta (i,j) kolla om det finns en lösning, om så finns ska denna 
+	private boolean solve(int i, int j) {
+		return true;
+	}
+	
 }
