7a9b1731681ee9704d464219c6daf7c26c87da67
HENRI <HENRI@DESKTOP-VTAKM6R>
2018-11-20 17:58:17 +0100

    kollar om nästa ruta finns och flytar till den

--- SudokuModel/src/sodukuModel/SudokuModel.java
+++ SudokuModel/src/sodukuModel/SudokuModel.java
@@ -2,31 +2,46 @@ package sodukuModel;
 
 public class SudokuModel {
 	private int[][] matrix;
-	
+
 	public SudokuModel() {
-		matrix=new int[9][9];
-		
+		matrix = new int[9][9];
+
 	}
-	
+
 	public boolean next() {
 		return true;
 	}
-	
+
 	public int getValue(int i, int j) {
 		return matrix[i][j];
 	}
-	
+
 	public void setValue(int i, int j, int value) {
-		matrix[i][j]=value;
+		matrix[i][j] = value;
 	}
-	
-	public void solve() {
-		solve(0,0);
+
+	public void finalSolve() {
+		if (solve(0, 0)) {
+//			sätt in alla värden i grafiska interfacet
+		}
+		
 	}
-	
+
 //	Ska för ruta (i,j) kolla om det finns en lösning, om så finns ska denna 
 	private boolean solve(int i, int j) {
-		return true;
+		
+		
+		if(matrix[i][j] != 0) {
+			if(i<8) {
+				solve(i+1, j);
+			} else if (j<8) {
+				solve(0, j+1);
+			} else {
+				return true;
+			}
+		}
+		
+
 	}
 	
 }
