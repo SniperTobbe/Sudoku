6de30afddd7dfd9125ae4a6b93f2999ebef46d1f
tfy15hbe <tfy15hbe@student.lu.se>
2018-12-06 14:31:25 +0100

    Sodukogame grundkonstruktion

--- SudokuModel/src/sodukuModel/SodukuGame.java
+++ SudokuModel/src/sodukuModel/SodukuGame.java
@@ -1,13 +1,25 @@
 package sodukuModel;
 import javafx.application.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class SodukuGame extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		BorderPane root = new BorderPane();
 		
+		Scene scene = new Scene(root, 600, 600);
+		primaryStage.setTitle("Soduko");
+		primaryStage.setScene(scene);
+		Button button1 = new Button("Solve");
+		Button button2 = new Button("Clear");
 		
 	}
 
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
